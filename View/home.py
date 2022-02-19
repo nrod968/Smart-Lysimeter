0c9f2a29cb5c1c8a5fb870d6fa8b341c34ff65f2
Nathaniel Rodriguez <golansy0@gmail.com>
2022-02-18 18:42:58 -0700

    First look at a MatPlotLib Chart

--- View/home.py
+++ View/home.py
@@ -5,6 +5,9 @@ from pathlib import Path
 from tkinter import *
 from view.window import SmartLysimeterWindow
 from utils.gui_tools import *
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk) 
 
 
 OUTPUT_PATH = Path(__file__).parent
@@ -16,7 +19,33 @@ def relative_to_assets(path: str) -> Path:
 
 class SmartLysimeterHome(SmartLysimeterWindow):
     def place(self, canvas: Canvas, root: Tk):
-        pass
+        self._graphFrame = Frame()
+        self._toolbarFrame = Frame()
+        fig = Figure(figsize = (5, 4), dpi = 100)
+    
+        # list of squares
+        y = [i**2 for i in range(101)]
+    
+        # adding the subplot
+        plot1 = fig.add_subplot(111)
+    
+        # plotting the graph
+        plot1.plot(y)
+    
+        # creating the Tkinter canvas
+        # containing the Matplotlib figure
+        canvas = FigureCanvasTkAgg(fig, master = self._graphFrame)  
+        canvas.draw()
+    
+        # placing the canvas on the Tkinter window
+        canvas.get_tk_widget().pack()
+    
+        # creating the Matplotlib toolbar
+        #toolbar = NavigationToolbar2Tk(canvas, self._toolbarFrame)
+        #toolbar.update()
+    
+        # placing the toolbar on the Tkinter window
+        self._graphFrame.place(x = 270, y = 70)
     
     def unplace(self):
         pass
