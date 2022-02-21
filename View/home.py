0f5153154caca9ee581277e56b216b4cde062502
Nathaniel Rodriguez <golansy0@gmail.com>
2022-02-20 21:20:28 -0700

    Added mockup title and axes labels

--- View/home.py
+++ View/home.py
@@ -1,6 +1,8 @@
 # from tkinter import *
 # Explicit imports to satisfy Flake8
 from tkinter import *
+
+from matplotlib.pyplot import tight_layout, xlabel, ylabel
 from view.window import SmartLysimeterWindow
 from utils.gui_tools import *
 from matplotlib.figure import Figure
@@ -11,13 +13,14 @@ class SmartLysimeterHome(SmartLysimeterWindow):
     def place(self, canvas: Canvas, root: Tk):
         self._graphFrame = Frame()
         self._toolbarFrame = Frame()
-        fig = Figure(figsize=(5, 3.75), dpi=100)
+        fig = Figure(figsize=(5, 3.75), dpi=100, tight_layout = True)
+        #fig.add_axes()
 
         # list of squares
         y = [i**2 for i in range(101)]
 
         # adding the subplot
-        plot1 = fig.add_subplot(111)
+        plot1 = fig.add_subplot(111, ylabel="Hello", xlabel="World", title="Hello World!")
 
         # plotting the graph
         plot1.plot(y)
@@ -28,8 +31,9 @@ class SmartLysimeterHome(SmartLysimeterWindow):
         canvas.draw()
 
         # placing the canvas on the Tkinter window
-        canvas.get_tk_widget().pack()
-        self._graphFrame.place(x=270, y=65)
+        #canvas.get_tk_widget().pack()
+        canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+        self._graphFrame.place(x=260, y=65)
 
         # creating the Matplotlib toolbar
         #toolbar = NavigationToolbar2Tk(canvas, self._toolbarFrame)
