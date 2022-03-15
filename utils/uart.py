46a07b6ee4b072fb094b43c30a2d93bc57023343
Nathaniel Rodriguez <golansy0@gmail.com>
2022-03-15 15:09:49 -0700

    added sample uart code

---
+++ utils/uart.py
@@ -0,0 +1,55 @@
+import serial
+import sys
+import time
+import string 
+from serial import SerialException
+
+def read_line(ser):
+	"""
+	taken from the ftdi library and modified to 
+	use the ezo line separator "\r"
+	"""
+	lsl = len(b'\r')
+	line_buffer = []
+	while True:
+		next_char = ser.read(1)
+		if next_char == b'':
+			break
+		line_buffer.append(next_char)
+		if (len(line_buffer) >= lsl and
+				line_buffer[-lsl:] == [b'\r']):
+			break
+	return b''.join(line_buffer)
+	
+def read_lines(ser):
+	"""
+	also taken from ftdi lib to work with modified readline function
+	"""
+	lines = []
+	try:
+		while True:
+			line = read_line()
+			if not line:
+				break
+				ser.flush_input()
+			lines.append(line)
+		return lines
+	
+	except SerialException as e:
+		print( "Error, ", e)
+		return None	
+
+def send_cmd(ser, cmd):
+	"""
+	Send command to the Atlas Sensor.
+	Before sending, add Carriage Return at the end of the command.
+	:param cmd:
+	:return:
+	"""
+	buf = cmd + "\r"     	# add carriage return
+	try:
+		ser.write(buf.encode('utf-8'))
+		return True
+	except SerialException as e:
+		print ("Error, ", e)
+		return None
