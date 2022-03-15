fe867418026c166fd682351897d4ea5cad6ebb29
Nathaniel Rodriguez <golansy0@gmail.com>
2022-03-15 11:38:03 -0700

    added getter and setter for num records and getter for last n records

--- model/model.py
+++ model/model.py
@@ -3,12 +3,19 @@ import csv
 
 class SmartLysimeterModel():
     fieldnames = ["date_time", "pH", "EC", "Drainage Rate"]
-    def __init__(self, dbFileName, csvFileName, n=10):
+    def __init__(self, dbFileName, csvFileName, numRecords=10):
         self._lastDataPoint = {}
         self._lastNDataPoints = {}
-        self._n = n
+        self._numRecords = numRecords
         self._db = TinyDB(dbFileName)
         self._csvFileName = csvFileName
+        self._currRecord = 0
+
+    def set_num_records(self, num_records):
+        self._numRecords = num_records
+
+    def get_num_records(self):
+        return self._numRecords
 
     def record_data_point(self, dateTime, phReading, ecReading, drainageReading):
         self._lastDataPoint =  {"date_time": dateTime,
@@ -24,7 +31,16 @@ class SmartLysimeterModel():
             writer.writeheader()
             writer.writerow(self._lastDataPoint)
     def save_last_reading_db(self):
-        self._db.insert(self._lastDataPoint)
+        self._currRecord = self._db.insert(self._lastDataPoint)
 
     def get_last_reading(self):
         return self._lastDataPoint
+
+    def get_last_n_readings(self):
+        readings = []
+        for i in range(self._numRecords):
+            if (i > self._numRecords):
+                break
+            readings.append(self._db.get(doc_id=(self._currRecord - i)))
+        readings.reverse()
+        return readings
