3204dab12283d4305c5b0fe27b6565d01fc88080
Nathaniel Rodriguez <golansy0@gmail.com>
2022-04-17 10:53:35 -0700

    Made level sensor use voltage divider formula

--- sensors/level_sensor.py
+++ sensors/level_sensor.py
@@ -3,6 +3,8 @@ from utils.mcp3008 import MCP3008
 
 class LevelSensor(SmartLysimeterSensor):
     RESISTOR_GRADIENT = 150
+    V_IN = 5
+    RESISTOR_VAL = 500
 
     def __init__(self, channelRef, channelSense):
         self._backend = MCP3008()
@@ -10,8 +12,10 @@ class LevelSensor(SmartLysimeterSensor):
         self._channelSense = channelSense
     
     def read(self):
-        refVal = self._backend.read(self._channelRef)
-        senseVal = self._backend.read(self._channelSense)
+        refVout = self._backend.read(self._channelRef)
+        refVal = (LevelSensor.RESISTOR_VAL / (refVout / LevelSensor.V_IN)) - LevelSensor.RESISTOR_VAL
+        senseVout = self._backend.read(self._channelSense)
+        senseVal = (LevelSensor.RESISTOR_VAL / (senseVout / LevelSensor.V_IN)) - LevelSensor.RESISTOR_VAL
         sensorValue = refVal - senseVal
         sensorValue = sensorValue / LevelSensor.RESISTOR_GRADIENT
     
