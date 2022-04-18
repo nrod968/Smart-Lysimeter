8811fca32e1d07ec23d499c91cf0aa122739bdcf
Nathaniel Rodriguez <golansy0@gmail.com>
2022-04-17 22:38:34 -0700

    Fixed MCP3008 adc conversion

--- sensors/level_sensor.py
+++ sensors/level_sensor.py
@@ -3,8 +3,9 @@ from utils.mcp3008 import MCP3008
 
 class LevelSensor(SmartLysimeterSensor):
     RESISTOR_GRADIENT = 150
-    V_IN = 5
-    RESISTOR_VAL = 500
+    V_IN = 3.3
+    V_REF = 3.3
+    RESISTOR_VAL = 560
 
     def __init__(self, channelRef, channelSense):
         self._backend = MCP3008()
@@ -12,9 +13,9 @@ class LevelSensor(SmartLysimeterSensor):
         self._channelSense = channelSense
     
     def read(self):
-        refVout = self._backend.read(self._channelRef)
+        refVout = (self._backend.read(self._channelRef) / MCP3008.RESOLUTION) * LevelSensor.V_REF
         refVal = (LevelSensor.RESISTOR_VAL / (refVout / LevelSensor.V_IN)) - LevelSensor.RESISTOR_VAL
-        senseVout = self._backend.read(self._channelSense)
+        senseVout = (self._backend.read(self._channelSense) / MCP3008.RESOLUTION) * LevelSensor.V_REF
         senseVal = (LevelSensor.RESISTOR_VAL / (senseVout / LevelSensor.V_IN)) - LevelSensor.RESISTOR_VAL
         sensorValue = refVal - senseVal
         sensorValue = sensorValue / LevelSensor.RESISTOR_GRADIENT
